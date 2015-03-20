0894ccc264bba3f12b8d3851f53ab91c2de1091f
CAThanatos <asriellursat@gmail.com>
2015-03-20 18:15:04 +0100

    StagHunt-WaypointsHunting

---
+++ exp/StagHunt-WaypointsHunting/BestLeadershipEval.hpp
@@ -0,0 +1,68 @@
+#ifndef BEST_LEADERSHIP_EVAL_
+#define BEST_LEADERSHIP_EVAL_
+
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <sferes/stat/stat.hpp>
+
+namespace sferes
+{
+  namespace stat
+  {
+    // assume that the population is sorted !
+    SFERES_STAT(BestLeadershipEval, Stat)
+    {
+    public:
+      template<typename E>
+				void refresh(const E& ea)
+      {
+				_best = *ea.pop().begin();
+
+				this->_create_log_file_leadership(ea, "bestleadership.dat");
+
+				if (ea.dump_enabled())
+				{
+					(*this->_log_file_leadership) << ea.nb_eval() << "," << _best->fit().value();
+
+          (*this->_log_file_leadership) << "," << _best->proportion_leader_preys();
+          float proportion_leader_preys_first = _best->nb_leader_first()/_best->nb_preys_killed_coop();
+          (*this->_log_file_leadership) << "," << proportion_leader_preys_first;
+          (*this->_log_file_leadership) << "," << _best->nb_leader_preys_first() << "," << _best->nb_preys_killed_coop();
+
+          (*this->_log_file_leadership) << "," << _best->proportion_leader_waypoints();
+          float proportion_leader_waypoints_first = _best->nb_leader_first()/_best->nb_preys_killed_coop();
+          (*this->_log_file_leadership) << "," << proportion_leader_waypoints_first;
+          (*this->_log_file_leadership) << "," << _best->nb_leader_waypoints_first() << "," << _best->nb_preys_killed_coop() << "," << std::endl;
+				}
+      }
+      void show(std::ostream& os, size_t k)
+      {
+				_best->develop();
+				_best->show(os);
+				_best->fit().set_mode(fit::mode::view);
+				_best->fit().eval_compet(*_best, *_best);
+      }
+      const boost::shared_ptr<Phen> best() const { return _best; }
+      template<class Archive>
+      void serialize(Archive & ar, const unsigned int version)
+      {
+        ar & BOOST_SERIALIZATION_NVP(_best);
+      }
+    protected:
+      boost::shared_ptr<Phen> _best;
+      
+      boost::shared_ptr<std::ofstream> _log_file_leadership;
+      
+      template<typename E>
+      void _create_log_file_leadership(const E& ea, const std::string& name)
+      {
+				if (!_log_file_leadership && ea.dump_enabled())
+				{
+					std::string log = ea.res_dir() + "/" + name;
+					_log_file_leadership = boost::shared_ptr<std::ofstream>(new std::ofstream(log.c_str()));
+				}
+      }
+    };
+  }
+}
+#endif
