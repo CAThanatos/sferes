72effe92ef65312b3326a1779c6eb90a59910f80
CAThanatos <asriellursat@gmail.com>
2015-10-15 14:18:30 +0200

    computeExpectation

--- scriptsArthur/computeExpectation.py
+++ scriptsArthur/computeExpectation.py
@@ -4,6 +4,7 @@ import argparse
 import math
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 
 
@@ -49,8 +50,13 @@ def main(args) :
 
 	dpi = 96
 	size = (1680/dpi, 1024/dpi)
-	fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, nrows=1 figsize = size)
+	# fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize = size)
 
+	fig = plt.figure(figsize = size)	
+	ax0 = plt.subplot2grid((2,3), (0,0))
+	ax1 = plt.subplot2grid((2,3), (0,1))
+	ax2 = plt.subplot2grid((2,3), (0,2))
+	ax3 = plt.subplot2grid((2,3), (1,0), colspan = 3)
 
 	# -- Solo VS Follower --
 	Yexpectation = []
@@ -95,6 +101,42 @@ def main(args) :
 	ax2.set_ylabel('Expected value')
 	ax2.set_ylim(0.0, 7000.0)
 	ax2.set_title('Expected value of turner individuals against solo')	
+
+
+	# -- Follower VS Solo VS Turner --
+	Zexpectation = []
+	tabProportions = {}
+	Yproportions = np.arange(0.0, 1.0, args.precision)	
+
+	for Xproportion in Xproportions :
+		tabProportions['Follower'] = Xproportion
+
+		tmpArray = []
+		for Yproportion in Yproportions : 
+			if Xproportion + Yproportion <= 1.0 :
+				tabProportions['Solo'] = Yproportion
+				tabProportions['Turner'] = 1.0 - Xproportion - Yproportion
+				tmpArray.append(computeExpectation('Follower', tabProportions))
+			else :
+				tmpArray.append(0.0)
+
+		Zexpectation.append(tmpArray)
+
+	Zexpectation = np.array(Zexpectation)
+	print(Zexpectation)
+	heatmap = ax3.imshow(Zexpectation, cmap = cm.jet)
+	plt.colorbar(heatmap)
+
+	ticks = range(0, len(Xproportions), len(Xproportions)/5)
+	ax3.set_xlabel('Proportion of solo')
+	ax3.set_xticks(ticks)
+	ax3.set_xticklabels([Yproportions[t] for t in ticks])
+	ax3.set_ylabel('Proportion of followers')
+	ax3.set_yticks(ticks)
+	ax3.set_yticklabels([Xproportions[t] for t in ticks])
+	ax3.invert_yaxis()
+	# ax3.set_ylim(0.0, 7000.0)
+	ax3.set_title('Expected value of follower individuals against solo and turners')	
 
 
 	plt.show()
